0c7fc94f53551a7d70e498a54a19aaec6eefec3f
Eric Jizba <erijiz@microsoft.com>
2024-05-22 11:36:26 -0700

    Form example

--- src/functions/streamRequest.ts
+++ src/functions/streamRequest.ts
@@ -5,7 +5,17 @@ app.http('streamRequest', {
     methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request: HttpRequest, context: InvocationContext) => {
-        await writeOutputFile(request.body);
+        const form = await request.formData();
+        for (const [key, value] of form.entries()) {
+            if (typeof value === 'string') {
+                context.log(`${key}: ${value}`);
+            } else {
+                await writeOutputFile(value.stream());
+                context.log(`File name: ${value.name}`);
+                context.log(`File size: ${value.size}`);
+                context.log(`File type: ${value.type}`);
+            }
+        }
 
         return {
             body: 'Done!',
--- src/scripts/streamRequest.ts
+++ src/scripts/streamRequest.ts
@@ -1,19 +1,21 @@
 import { createReadStream } from 'fs';
-import { fetch } from 'undici';
-import { inputFilePath, sendRequestInChunks } from '../constants';
-import { readFile } from 'fs/promises';
+import { fetch, FormData } from 'undici';
+import { inputFilePath } from '../constants';
 
 async function streamRequest(): Promise<void> {
     const start = Date.now();
 
+    const form = new FormData();
+    form.append('string1', 'value1');
+    // https://stackoverflow.com/questions/75793118/streaming-multipart-form-data-request-with-native-fetch-in-node-js/75795888
+    form.set('file1', {
+        [Symbol.toStringTag]: 'File',
+        name: 'input.txt',
+        stream: () => createReadStream(inputFilePath),
+    });
+
     const url = 'http://127.0.0.1:7071/api/streamRequest';
-    if (sendRequestInChunks) {
-        const body = createReadStream(inputFilePath);
-        await fetch(url, { method: 'POST', body, duplex: 'half' });
-    } else {
-        const body = await readFile(inputFilePath);
-        await fetch(url, { method: 'POST', body });
-    }
+    await fetch(url, { method: 'POST', body: form });
 
     const duration = (Date.now() - start) / 1000;
     console.log(`streamRequest finished in ${duration} seconds`);
